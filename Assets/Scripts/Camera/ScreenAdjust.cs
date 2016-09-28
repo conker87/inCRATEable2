f0fadd2435f96047dd98a8bcfb327c976007f2ab
Simon Fletcher <sjfletcherhtc@gmail.com>
2016-09-28 22:03:01 +0100

    no message

---
+++ Assets/Scripts/Camera/ScreenAdjust.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]	
+public class ScreenAdjust : MonoBehaviour {
+
+	[Range(10, 2560)]
+	public float targetHeight = 1024f, targetWidth = 600f;
+
+	public bool useUpdate = false;
+
+	void Start () {
+	
+		Camera.main.aspect = targetWidth / targetHeight;
+
+	}
+
+	void Update() {
+
+		if (useUpdate) {
+
+			Camera.main.aspect = targetWidth / targetHeight;
+
+		}
+
+	}
+}
